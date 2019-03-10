940635fed276bc472a3222a692879ff2115135ea
Krishna Vaidyanathan <krishnarules@gmail.com>
2019-03-10 19:47:44 -0400

    Day 1 part 2

--- DayOne.kt
+++ DayOne.kt
@@ -1,16 +1,52 @@
 package com.krishna.aoc2018
 
+import java.math.BigInteger
 import java.io.File
 import java.io.File.separator as SEPARATOR
 
 class DayOne {
-    var freq = 0f
-    val file : File = File(".${SEPARATOR}input${SEPARATOR}day1.txt")
-    
-    fun computeFreq() {
-        freq = 0f
+    var freq: BigInteger 
+    var freqTwice : BigInteger?
+    var valList: List<BigInteger>
+    var freqSeenSet: MutableSet<BigInteger>
+
+    init {
+        freq = BigInteger.valueOf(0L)
+        freqTwice = null
+        freqSeenSet = mutableSetOf()
+        val valListAux: MutableList<BigInteger> = mutableListOf()
+        val file = File(".${SEPARATOR}input${SEPARATOR}day1.txt")
+
         file.forEachLine {
-            freq = freq + it.toFloat()
+           valListAux.add(it.toBigInteger())
+        }
+        valList = valListAux.toList()
+    }
+
+    fun computeFreq() {
+        freq = BigInteger.valueOf(0L)
+
+        for (value in valList) {
+            freq = freq + value
+        }
+    }
+
+    fun computeFirstRepeatedFreq() {
+        freq = BigInteger.valueOf(0L)
+        while(freqTwice == null)
+            computeFirstRepeatedFreqAux()
+    }
+    
+    fun computeFirstRepeatedFreqAux() {
+        for (value in valList) {
+            freq = freq + value
+
+            if (freqSeenSet.contains(freq)) {
+                freqTwice = freq
+                break
+            }
+            else
+                freqSeenSet.add(freq)
         }
     }
 }
@@ -18,6 +54,8 @@ class DayOne {
 fun main() {
     val dayOne = DayOne()
     dayOne.computeFreq()
+    println("Part 1: " + dayOne.freq.toString())
 
-    println(dayOne.freq)
+    dayOne.computeFirstRepeatedFreq()
+    println("Part 2: " + dayOne.freqTwice.toString())
 }
